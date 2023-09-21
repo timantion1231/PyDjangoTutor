624b1d810ded6aaac95cdf3e3aaa1b37addf4e4c
Khanafin Timur <mindiisky@yandex.ru>
2023-09-21 20:07:12 +0300

    rename & refactor

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-import ex1
+import les1
 
 if __name__ == '__main__':
     proga = -1
     while proga != 0:
         proga = int(input("enter the ex num\n1: ex1\n2: ex2\n0: exit\n"))
         if proga == 1:
-            ex1.exs.ex1()
+            les1.exs.ex1()
         elif proga == 2:
-            ex1.exs.ex2()
+            les1.exs.ex2()
 
